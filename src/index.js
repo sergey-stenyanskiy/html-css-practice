917d15d3378a5c122ab8e31cc1e31e8619e5af0d
Sergey Stenyanskiy <s.stenyansky@worksolutions.ru>
2021-10-06 14:55:30 +0300

    removed unnecessary js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,1 @@
-// import "./css/index.scss"
-// import "./css/index.css"
 import "./css/index.scss"
-
-// const business = document.querySelector('.business');
-// const dropdown = document.querySelector('.dropdown');
-
-
-// business.addEventListener('mouseover', function(e) {
-//   dropdown.style.display = 'block';
-// });
-
-// business.addEventListener('mouseout', function(e) {
-//   dropdown.style.display = 'none';
-// })
